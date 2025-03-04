be2106488cabcaf92b2d2e75d67db56ef6bd2be5
Franco Fraticelli <ffraticelli@ycspr.net>
2025-03-04 01:40:46 -0300

    link to dates route added

--- src/app/routes/organization/subroutes/season/season.component.ts
+++ src/app/routes/organization/subroutes/season/season.component.ts
@@ -1,46 +1,57 @@
-import { Component, Input, OnInit, Signal, computed, inject, signal } from '@angular/core'
+import {
+    Component,
+    Input,
+    OnInit,
+    Signal,
+    computed,
+    inject,
+    signal,
+} from '@angular/core'
 import { BasketAPIService } from '../../../../services/basketAPI/basket-api.service'
 import { OrganizationsStore } from '../../../../stores/organizations/organizations.store'
 import { EMPTY, catchError } from 'rxjs'
 import { IDate } from '../../../../services/basketAPI/basket-api.types'
 import { OrganizationComponent } from '../../organization.component'
 import { TOrganizationStore } from '../../../../stores/organizations/organizations.store.types'
-import { ActivatedRoute } from '@angular/router'
+import { ActivatedRoute, RouterLink } from '@angular/router'
 
 @Component({
     selector: 'app-season',
     standalone: true,
-    imports: [],
+    imports: [RouterLink],
     providers: [BasketAPIService, OrganizationComponent],
     templateUrl: './season.component.html',
     styleUrl: './season.component.css',
 })
 export class SeasonComponent implements OnInit {
-    constructor(private basketAPI: BasketAPIService, private activatedRoute: ActivatedRoute) { }
+    constructor(
+        private basketAPI: BasketAPIService,
+        private activatedRoute: ActivatedRoute
+    ) {}
 
     @Input() organizationId = ''
     @Input() seasonId = ''
 
     private organizationStore = inject(OrganizationsStore)
 
-    organizations: Signal<TOrganizationStore> = computed(() => this.organizationStore.organizations())
+    organizations: Signal<TOrganizationStore> = computed(() =>
+        this.organizationStore.organizations()
+    )
 
     dates = signal<Array<IDate> | null>(null)
 
     ngOnInit(): void {
-        const organizationId = this.activatedRoute.parent?.snapshot.paramMap.get('organizationId') as string
+        const organizationId =
+            this.activatedRoute.parent?.snapshot.paramMap.get(
+                'organizationId'
+            ) as string
         const seasonId = this.seasonId
 
-        const seasons =
-            // this.organization.seasons()
-            this.organizations()[organizationId]?.seasons
-
-
-        console.log(this.organizations()[Number(organizationId)])
+        const seasons = this.organizations()[organizationId]?.seasons
         const regularPhaseId =
             seasons && seasons.length
                 ? seasons.find((season) => season.id === seasonId)
-                    ?.regularPhaseId
+                      ?.regularPhaseId
                 : null
 
         if (!regularPhaseId) {
